e41d7d84bfe8156f835db0a5f218fb333c9fef75
devarshsanghvi <devarshsanghvi.ds@gmail.com>
2023-10-15 17:06:55 +0530

    added clean cache in setup

--- setup.bat
+++ setup.bat
@@ -6,10 +6,12 @@ git clone https://github.com/parthshah7551/news-feed-extractor-frontend.git
 
 cd news-feed-extractor-worker
 call setup.bat
+deactivate
 
 cd ../news-feed-extractor-backend
 npm i
 
 cd ../news-feed-extractor-frontend
+npm cache clean --force
 npm i 
 PAUSE
