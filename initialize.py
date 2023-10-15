f7cc33083cea1fd27c8e63f7be95ffa95a269fde
devarshsanghvi <devarshsanghvi.ds@gmail.com>
2023-10-15 18:27:22 +0530

    changed order of processes

--- initialize.py
+++ initialize.py
@@ -2,7 +2,7 @@ import psutil
 import webbrowser
 import socket
 import sys
-
+import time
 
 def find_available_port():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -61,6 +61,14 @@ FLASK_APP_DIR = "./news-feed-extractor-worker"
 #     stderr=subprocess.PIPE,
 #     text=True,
 # )
+flask_server = subprocess.Popen(
+    f'cd {FLASK_APP_DIR} && "env/Scripts/activate" && set NEWS_EXTRACTOR_REACT_PORT={react_port} && set NEWS_EXTRACTOR_NODE_PORT={node_port} && flask run --port={flask_port}',
+    shell=True,
+    # stdout=subprocess.PIPE,
+    # stderr=subprocess.PIPE,
+    text=True,
+)
+
 
 react_server = subprocess.Popen(
     f"cd {REACT_APP_DIR} && set REACT_APP_NEWS_EXTRACTOR_NODE_PORT={node_port}&& set PORT={react_port}&&set REACT_APP_NEWS_EXTRACTOR_FLASK_PORT={flask_port}&& npm start",
@@ -78,21 +86,13 @@ node_server = subprocess.Popen(
     text=True,
 )
 
-flask_server = subprocess.Popen(
-    f'cd {FLASK_APP_DIR} && "env/Scripts/activate" && set NEWS_EXTRACTOR_REACT_PORT={react_port} && set NEWS_EXTRACTOR_NODE_PORT={node_port} && flask run --port={flask_port}',
-    shell=True,
-    # stdout=subprocess.PIPE,
-    # stderr=subprocess.PIPE,
-    text=True,
-)
-
 print("react_port", react_port, "node_port", node_port, "flask_port", flask_port)
 # if node_server.poll() is None:
 #     print("Process is still running.")
 # node_server.kill()
 
 react_url = f"http://localhost:{flask_port}"
-# time.sleep(4)
+time.sleep(1)
 webbrowser.open(react_url)
 # print(node_server)
 # print(react_server)
