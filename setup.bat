3bac2eb9e6d7af0af4b80049db53d0fc76e9ce4e
devarshsanghvi <devarshsanghvi.ds@gmail.com>
2023-10-15 16:22:23 +0530

    renamed flask folder

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ git clone https://github.com/Devarsh-leo/news-feed-extractor-worker.git
 git clone https://github.com/parthshah7551/news-feed-extractor-backend.git
 git clone https://github.com/parthshah7551/news-feed-extractor-frontend.git
 
-cd news-feed-extractor
+cd news-feed-extractor-worker
 call setup.bat
 
 cd ../news-feed-extractor-backend
